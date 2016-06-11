79cbb6b29c053605037a4262e2492fee1b432a65
daleroy <david.a.leroy@gmail.com>
2016-06-11 18:21:41 +0000

    createVirtualHost function complete.
    createVirtualHost will read the domain name you want to create a
    vhosts for and use the middleware function that you provide in
    the sites directory to handle the routing. Basic setup handles GET
    request only.

--- src/server/createVirtualHost.js
+++ src/server/createVirtualHost.js
@@ -1,6 +1,11 @@
-var express = require('express');
 var vhost = require('vhost');
+var sites = global.appRoot + '/sites/';
 
-module.exports = function(domainName, dirPath) {
-    return vhost(domainName, express.static(dirPath));
+module.exports = function(domainName, middleware) {
+    var handler = require(sites + middleware);
+
+    if (handler && typeof handler === 'function') { 
+        console.log('Created vHost for domainName: %s middleware: %s.', domainName, middleware);
+        return vhost(domainName, handler);
+    }
 }
