9baedea8554ef80e426b71564f3dbabd933110aa
daleroy <david.a.leroy@gmail.com>
2016-06-14 04:51:33 +0000

    Adding createVirtualHost module to create vhost for a static directory and use routes middleware.

--- src/server/createVirtualHost.js
+++ src/server/createVirtualHost.js
@@ -1,11 +1,23 @@
+var ROUTES_PATH = '/src/server/routes';
 var vhost = require('vhost');
+var express = require('express');
 var sites = global.appRoot + '/sites/';
 
-module.exports = function(domainName, middleware) {
-    var handler = require(sites + middleware);
+module.exports = {
+    createRoutes: function(domainName, middleware) {
+        var routes = require(sites + middleware + ROUTES_PATH);
 
-    if (handler && typeof handler === 'function') { 
-        console.log('Created vHost for domainName: %s middleware: %s.', domainName, middleware);
-        return vhost(domainName, handler);
+        if (routes && typeof routes === 'function') { 
+            console.log('Created vHost for domainName: %s using middleware: %s.', domainName, middleware);
+
+            return vhost(domainName, routes);
+        }
+    },
+    createStatic: function(domainName, middleware) {
+        var staticPath = sites + middleware + '/public';
+
+        console.log('Created vHost for domainName: %s using static path: %s.', domainName, staticPath);
+
+        return vhost(domainName, express.static(staticPath));
     }
 }
