1a27003d64c16f643f19d8d267017f3d81c87855
daleroy <david.a.leroy@gmail.com>
2016-06-14 05:18:27 +0000

    Adding compression module for requests.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 global.appRoot = __dirname;
 var sites = global.appRoot + '/sites/';
 var express = require('express');
+var compression = require('compression');
 var settings = require('./src/settings');
 var cvh = require('./src/server/createVirtualHost.js');
 var vhost = require('vhost');
 var main = express();
 
+main.use(compression());
 settings.hosts.forEach(function(host) {
     console.log('Attempting to create Virtual Host for url: %s using middleware: %s.', host.url, host.middleware);
 
