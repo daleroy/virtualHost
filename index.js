2caa8c507046d220905debb6947b52151b7dcbf7
daleroy <david.a.leroy@gmail.com>
2016-06-19 18:48:07 +0000

    Adding middleware to log requests.wq

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ var vhost = require('vhost');
 var main = express();
 
 main.use(compression());
+main.use(function(req, res, next) {
+    console.log('********** request received **********');
+    console.log('req.method: ' + req.method);
+    console.log('req.hostname: ' + req.hostname);
+    console.log('req.path' + req.path);
+    console.log('**************************************');
+    next();
+});
 settings.hosts.forEach(function(host) {
     console.log('Attempting to create Virtual Host for url: %s using middleware: %s.', host.url, host.middleware);
 
